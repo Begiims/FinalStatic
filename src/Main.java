821bdee37e3c2c9fa2caa9ad1f5dca5457155394
Begiims <attokurovabegimaj@gmail.com>
2022-12-15 18:42:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Student pupil = new Student();
+        pupil.name = "Aidana";
+        pupil.surname = "Bolotbekova";
+        pupil.study = "9 A";
+        pupil.age = 15;
+        pupil.school = "№ 14";
+        System.out.println("name:"+pupil.name+"\n"+"surname:"+pupil.surname+"\n"+
+                "study:"+pupil.study+"\n"+"age:"+pupil.age+"\n"+ "shool:"+pupil.school);
+        }
+    }
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    String name;
+    String surname;
+    String study;
+    int age;
+    String school;
+
+}
